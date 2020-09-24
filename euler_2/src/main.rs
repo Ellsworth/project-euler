24e6b939eb137f996fc97adef3286e9eb4f3a6b7
Erich Ellsworth <kg5key@kg5key.com>
2020-09-24 18:43:12 -0500

    Rough Fibonacci sequence generator

--- euler_2/src/main.rs
+++ euler_2/src/main.rs
@@ -1,3 +1,17 @@
+use std::{thread, time};
+
 fn main() {
+    let mut f: [i64; 3] = [0, 1, 0];
+
+    while f[1] <= 100 {
+        f[2] = f[0] + f[1];
+        f[0] = f[1];
+        f[1] = f[2];
+        //println!("array is {:?}",f);
+        println!("{}", f[0]);
+        thread::sleep(time::Duration::from_secs(1));
+
+    }
+
     
 }
