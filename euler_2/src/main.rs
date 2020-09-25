03bbec270ea976a45a130fefed2aa712db23769c
Erich Ellsworth <kg5key@kg5key.com>
2020-09-24 19:01:29 -0500

    Sum now works.
    We are going one step over when finding  next sequence.

--- euler_2/src/main.rs
+++ euler_2/src/main.rs
@@ -1,6 +1,7 @@
-use std::{thread, time};
+//use std::{thread, time};
 
 fn main() {
+    let mut sum = 0;
     let mut f: [i64; 3] = [0, 1, 0];
 
     while f[1] <= 100 {
@@ -8,8 +9,15 @@ fn main() {
         f[0] = f[1];
         f[1] = f[2];
         //println!("array is {:?}",f);
+        
+        //thread::sleep(time::Duration::from_secs(1));
+
+        if f[1] % 2 == 0 {
+            sum += f[1];
+            println!("{} is even. Sum: {}", f[1], sum);
+            continue;
+        }
         println!("{}", f[0]);
-        thread::sleep(time::Duration::from_secs(1));
 
     }
 
