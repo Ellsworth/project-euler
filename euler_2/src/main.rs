9cf325c334884db8b70c05ef9922243f7c64e6a5
Erich Ellsworth <kg5key@kg5key.com>
2020-09-24 20:10:21 -0500

    Working solution for problem 2

--- euler_2/src/main.rs
+++ euler_2/src/main.rs
@@ -1,25 +1,25 @@
 //use std::{thread, time};
 
 fn main() {
+    let max = 4000000;
     let mut sum = 0;
     let mut f: [i64; 3] = [0, 1, 0];
 
-    while f[1] <= 100 {
+    while f[1] < max {
         f[2] = f[0] + f[1];
         f[0] = f[1];
         f[1] = f[2];
-        //println!("array is {:?}",f);
-        
-        //thread::sleep(time::Duration::from_secs(1));
+
 
         if f[1] % 2 == 0 {
             sum += f[1];
             println!("{} is even. Sum: {}", f[1], sum);
             continue;
         }
-        println!("{}", f[0]);
+        
 
     }
+    println!("Sum: {}", sum)
 
     
 }
